18391c588d130c7644f153b8f325f1dbc2b101f3
Shivansh Rai <shivansh@freebsd.org>
2018-01-31 18:20:32 +0530

    grammar: Add support for pencilled entries ; Update tests

--- grammar.py
+++ grammar.py
@@ -2,20 +2,28 @@
 Grammar definitions
 """
 
+def lookup(k, l):
+    """
+    lookup k in a list l, which can also have pencilled entries. A
+    pencilled entry is a list of possible values a cell might contain.
+    """
+    for elem in l:
+        if isinstance(elem, list):  # pencilled entry
+            for x in elem:
+                if x == k:
+                    return True
+        elif elem == k:
+            return True
+    return False
+
 def row(mat, i, k):
     """Returns true if the i'th row contains k"""
-    if k in mat[i]:
-        return True
-    return False
+    return lookup(k, mat[i])
 
 def col(mat, i, k):
     """Returns true if the i'th column contains k"""
-    if k in [row[i] for row in mat]:
-        return True
-    return False
+    return lookup(k, [row[i] for row in mat])
 
 def grid(mat, i, j, k):
     """Returns true if the specified grid contains k"""
-    if k in [ mat[i + p][j + q] for p in range(3) for q in range(3) ]:
-        return True
-    return False
+    return lookup(k, [ mat[i + p][j + q] for p in range(3) for q in range(3) ])
--- test.py
+++ test.py
@@ -3,14 +3,14 @@ import grammar
 
 class TestStringMethods(unittest.TestCase):
        def test_grammar(self):
-              mat = ((2,4,8),
-                     (1,2,3),
-                     (3,5,7))
-              self.assertEqual(grammar.row(mat, 0, 8), True)
-              self.assertEqual(grammar.row(mat, 1, 4), False)
-              self.assertEqual(grammar.col(mat, 2, 7), True)
+              mat = ((2,[1,4],8),
+                     (1,  2  ,3),
+                     (3,  5  ,7))
+              self.assertEqual(grammar.row(mat, 0, 1), True)
+              self.assertEqual(grammar.row(mat, 0, 3), False)
+              self.assertEqual(grammar.col(mat, 1, 1), True)
               self.assertEqual(grammar.col(mat, 1, 3), False)
-              self.assertEqual(grammar.grid(mat, 0, 0, 5), True)
+              self.assertEqual(grammar.grid(mat, 0, 0, 4), True)
               self.assertEqual(grammar.grid(mat, 0, 0, 9), False)
 
 if __name__ == '__main__':
